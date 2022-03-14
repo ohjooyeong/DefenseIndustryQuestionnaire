1e7125ee4506300f28de2a255fd4c7d038119877
OH-JOO-YEONG <brb1111@naver.com>
2022-03-14 23:09:33 +0900

    [Feat] Period Table 구현

--- front/src/components/SupportBusiness/PeriodTable/index.jsx
+++ front/src/components/SupportBusiness/PeriodTable/index.jsx
@@ -1,48 +1,10 @@
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import React from "react";
-function SupportBusiness() {
-  const tmpData = {
-    start: 4,
-    end: 12,
-    title: "연구개발 능력 향상",
-  };
-  const renderTableBody = (data) => {
-    const rows = [];
 
-    for (let i = 1; i < 13; i++) {
-      if (data.start === i) {
-        let len = 0;
-        len = data.end - data.start + 1;
-
-        rows.push(
-          <td>
-            <DivPeriod length={len * 28.9 - 8}>
-              <p>모집공고</p>
-            </DivPeriod>
-          </td>
-        );
-      } else {
-        rows.push(<td></td>);
-      }
-    }
-    return (
-      <tr>
-        <td>
-          <p>{data.title}</p>
-        </td>
-        {rows}
-      </tr>
-    );
-  };
-
+function PeriodTable() {
   return (
-    <Container>
-      <Title>지원프로그램 및 사업 소개</Title>
-      <Desc>
-        협약기업 분석 및 해결방안에 따른 지원 프로그램과 사업을 소개하여, 기업의
-        애로사항을 해소하도록 돕습니다.
-      </Desc>
+    <>
       <SubTitle color={"#f8d1b6"}>
         <div></div>교육 및 컨설팅
       </SubTitle>
@@ -133,101 +95,11 @@ function SupportBusiness() {
           </tr>
         </tbody>
       </Table>
-      <SubTitle color={"#9dc3e6"}>
-        <div></div>방위산업 지원 사업
-      </SubTitle>
-      <Table>
-        <thead>
-          <tr>
-            <th></th>
-            <th>
-              <p>1월</p>
-            </th>
-            <th>
-              <p>2월</p>
-            </th>
-            <th>
-              <p>3월</p>
-            </th>
-            <th>
-              <p>4월</p>
-            </th>
-            <th>
-              <p>5월</p>
-            </th>
-            <th>
-              <p>6월</p>
-            </th>
-            <th>
-              <p>7월</p>
-            </th>
-            <th>
-              <p>8월</p>
-            </th>
-            <th>
-              <p>9월</p>
-            </th>
-            <th>
-              <p>10월</p>
-            </th>
-            <th>
-              <p>11월</p>
-            </th>
-            <th>
-              <p>12월</p>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <p>연구개발 능력 향상</p>
-            </td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          {renderTableBody(tmpData)}
-        </tbody>
-      </Table>
-    </Container>
+    </>
   );
 }
 
-export default SupportBusiness;
-
-const Container = styled("div")`
-  padding-left: 20px;
-  padding-right: 16px;
-`;
-
-const Title = styled("h2")`
-  font-weight: 800;
-  font-size: 16px;
-  line-height: 25px;
-  letter-spacing: -0.0025em;
-
-  color: #212121;
-  margin-bottom: 4px;
-`;
-
-const Desc = styled("div")`
-  font-size: 11px;
-  line-height: 18px;
-  letter-spacing: -0.0025em;
-
-  color: #616161;
-  margin-bottom: 10px;
-`;
+export default PeriodTable;
 
 const SubTitle = styled("h4")`
   display: flex;
